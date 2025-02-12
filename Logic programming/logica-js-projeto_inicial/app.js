bd563224862421f3c9612cb6674be11538484b9c
Ryan Mota <159202875+Motak7@users.noreply.github.com>
2025-02-12 00:00:12 -0300

    Delete Logic programming/logica-js-projeto_inicial directory

--- Logic programming/logica-js-projeto_inicial/app.js
+++
@@ -1,9 +0,0 @@
-alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 30");
-
-if (parseInt(chute) === numeroSecreto) {
-  alert("Isso aí! Você descobriu o número secreto (5)");
-} else {
-  alert("Que pena! Você errou. Tente novamente.");
-}
